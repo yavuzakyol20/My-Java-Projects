d02f182fa15b1259fe4ab226907a6ef4ffdbeb97
Yavuz Akyol <61031463+yavuzakyol20@users.noreply.github.com>
2024-03-14 01:08:01 +0100

    Delete My Java Projects directory

--- My Java Projects/Simple Circle Formulas and Calculation for products/Circle_Formulas.java
+++
@@ -1,8 +0,0 @@
-public class Circle_Formulas {
-    public static void main(String[] args) {
-        double r = 23.5; // radius is 23.5
-        System.out.println("Arealet av sirkelen med radius " + r + " er " + String.format("%.2f", Sirkel.areal(r)));
-        System.out.println("Omkretsen av sirkelen med radius " + r + " er " + String.format("%.2f", Sirkel.omkrets(r)));
-        System.out.println("Diameteren til sirkelen er med radius " + r + " er " + String.format("%.2f", Sirkel.diameter(r)));
-    }
-}
